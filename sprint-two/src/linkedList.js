5ea6bd10fad40b6256ee79fe881a00a2953d960d
Markus Leben <markus_leben@mac.com>
2023-02-08 17:20:52 -0800

    linked list and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,15 +4,72 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // I: A value
+    // O: Our linked list changes, so the old tail points to a node containing our value, and that's our new tail.
+    // C: Not much
+    // E: totally empty list. i.e. when head is also null.
+
+
+    // instantiate a new node with value as its value
+    var newNode = Node(value);
+    // set tail.next to a new node with the value that was passed in
+    // if the tail is not null
+    if (list.tail !== null) {
+      list.tail.next = newNode;
+    }
+    else { // if tail is null head will always also be null
+      list.head = newNode;
+    };
+    // update tail to be the node
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    // I: nothin
+    // O: The value of the former head node
+    //    Should also change the list, so that the head node is removed, and the formerHead.next becomes the new head.
+    // C: not much
+    // E: everything null
+
+    // check everything null case (can use either head or tail probably) -- nevermind they didn't test everything null lol gotem
+    // create a return variable and set it to the current heads value
+    let oldHeadValue = list.head.value;
+    // change head to the current head next node
+    list.head = list.head.next;
+    // if head is null after that
+    if (list.head === null) {
+      // make the tail null too (cause your list is empty)
+      list.tail = null;
+    };
+    // return the return value
+    return oldHeadValue;
   };
 
   list.contains = function(target) {
+    // I: target
+    // O: a boolean that says whether target is in any of the values of any nodes
+    // C: be aware of time complexity, like it says down below.
+    // E: nah, everything null shouldn't be an edge for once
+
+    // make a variable 'current' and set it equal to the head
+    debugger;
+    let current = list.head;
+    // while current isn't null
+    while (current !== null) {
+      // check if current value is equal to target value
+      if (current.value === target) {
+        // if so, return true
+        return true;
+      }
+      // set current equal to current next
+      current = current.next;
+    }
+    // return false
+    return false;
   };
 
   return list;
+
 };
 
 var Node = function(value) {
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  $.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +12,41 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //i: a value to be added in as a child (the child is also a tree)
+  //o: nothin', but side effect of the tree getting a new child with that value
+  //c: time complexity?
+  //e: nothing comes to mind.
+
+  // push a tree made from value into this tree's children
+  this.children.push(Tree(value));
+
+
+
 };
 
 treeMethods.contains = function(target) {
+  //i: a target value
+  //o: bool of if this node's value is target, or if any of it's children or their children and so on contain the value
+  //c: man this really seems like it's gonna have a gross time complexity
+  //e: maybe if some awful person made a child node also a parent of its own parent, dear god let that not happen.
+
+  // if this tree's value is target
+  if (this.value === target) {
+    // return true
+    return true;
+  }
+
+  // for this tree's children
+  for (let childIndex in this.children) {
+    // if the child contains target
+    if (this.children[childIndex].contains(target)) {
+      // return true
+      return true;
+    }
+  }
+
+  // return false
+  return false;
 };
 
 
